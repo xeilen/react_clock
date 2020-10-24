2017bd4d0ddadd59aae1ef0f8dff643a0b9369ac
38063 <xeilen1112@gmail.com>
2020-10-25 01:24:12 +0300

    Updates

---
+++ precache-manifest.33ea8f3a134bd01c031d8c3a6f8cd458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8610b1e9b595ac8e9e26b3a5783db314",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e6fb873f1045a979cbb",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6fb873f1045a979cbb",
+    "url": "./static/js/main.15e89f59.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
